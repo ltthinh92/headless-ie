8725f2b8e663ea6131bb09b35928a91a8b7b68a8
Thinh Le <ltthinh92@gmail.com>
2017-04-03 23:12:06 +0700

    upload source

---
+++ IE/IE/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IE
+{
+    public partial class frmMain : Form
+    {
+        private string _url;
+        public frmMain(string url)
+        {
+            InitializeComponent();
+            if (!string.IsNullOrEmpty(url))
+            {
+                _url = url;
+            }
+            else
+            {
+                _url = "https://www.google.com";
+            }
+            wbMain.Url = new UriBuilder(_url).Uri;
+        }
+
+        private void wbMain_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            frmMain.ActiveForm.Text = wbMain.Document.Title;
+        }
+    }
+}
---
+++ IE/IE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace IE
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string url = "";
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+            Application.Run(new frmMain(url));
+        }
+    }
+}
